e96d59cc1a9e041576c37985ac749e34c0ee826c
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2024-02-15 02:10:37 +0700

    add(register): add view signup

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -4,10 +4,10 @@ import React from 'react';
 
 export default function Login() {
   return (
-    <section className='relative flex justify-center items-center h-screen'>
+    <section className='flex justify-center items-center h-screen'>
       <div className=''>
         <Image src='/images/illustration.png' width={310} height={187} className='mx-auto' alt='illustration' />
-        <form className='flex flex-col mt-10 gap-y-4'>
+        <form className='flex flex-col mt-8 gap-y-4'>
           <div className='text-center mb-4'>
             <h3 className='text-2xl font-semibold'>Sign In</h3>
             <p className='text-[#887E7E]'>Sign in to continue!</p>
@@ -36,17 +36,17 @@ export default function Login() {
         </form>
         <p className='text-center mt-2'>
           Don&apos;t have account?{' '}
-          <Link href='#' className='text-[#FDE047] font-semibold'>
+          <Link href='register' className='text-[#FDE047] font-semibold'>
             Sign Up
           </Link>
         </p>
-        <div className='absolute bottom-0 inset-x-0 w-full flex gap-x-4 text-xs'>
-          <div className='grow w-full h-1 my-auto bg-gradient-to-l from-yellow-primary'></div>
-          <div className=''>
-            <p>&copy;TokoKu</p>
-          </div>
-          <div className='grow w-full h-1 my-auto bg-gradient-to-r from-yellow-primary'></div>
+      </div>
+      <div className='absolute bottom-12 inset-x-0 w-full flex gap-x-4 text-xs'>
+        <div className='grow w-full h-1 my-auto bg-gradient-to-l from-yellow-primary'></div>
+        <div className=''>
+          <p>&copy;TokoKu</p>
         </div>
+        <div className='grow w-full h-1 my-auto bg-gradient-to-r from-yellow-primary'></div>
       </div>
     </section>
   );
---
+++ src/app/(auth)/register/page.jsx
@@ -0,0 +1,92 @@
+import Link from 'next/link';
+import React from 'react';
+
+export default function Register() {
+  return (
+    <section>
+      <div className=''>
+        <h2 className='text-2xl text-black font-semibold'>Sign Up</h2>
+        <p className='text-[#635C5C]'>create account to continue</p>
+      </div>
+      <form className='flex flex-col mt-8 gap-y-4'>
+        <div className='form-group'>
+          <label htmlFor='' className='font-semibold text-lg text-black'>
+            Fullname
+          </label>
+          <input
+            type='text'
+            name='nama_pelanggan'
+            className='w-full p-4 border-2 rounded-lg border-[#887E7E]'
+            placeholder='Enter your fullname'
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='' className='font-semibold text-lg text-black'>
+            Email
+          </label>
+          <input
+            type='email'
+            name='email'
+            className='w-full p-4 border-2 rounded-lg border-[#887E7E]'
+            placeholder='Enter your email'
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='' className='font-semibold text-lg text-black'>
+            Username
+          </label>
+          <input
+            type='text'
+            name='username'
+            className='w-full p-4 border-2 rounded-lg border-[#887E7E]'
+            placeholder='Enter your username'
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='' className='font-semibold text-lg text-black'>
+            Mobile Number
+          </label>
+          <input
+            type='tel'
+            name='no_telepon'
+            className='w-full p-4 border-2 rounded-lg border-[#887E7E]'
+            placeholder='Enter your mobile number'
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='' className='font-semibold text-lg text-black'>
+            Password
+          </label>
+          <input
+            type='password'
+            name='password'
+            className='w-full p-4 border-2 border-[#887E7E] rounded-lg'
+            placeholder='Enter your password'
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='' className='font-semibold text-lg text-black'>
+            Confirm Password
+          </label>
+          <input
+            type='password'
+            name='confirm_password'
+            className='w-full p-4 border-2 border-[#887E7E] rounded-lg'
+            placeholder='Enter your password'
+          />
+        </div>
+        <div className='form-group'>
+          <button type='submit' className='w-full p-4 text-lg font-semibold rounded-lg bg-[#FDE047]'>
+            Sign Up
+          </button>
+        </div>
+      </form>
+      <p className='text-center mt-2'>
+        Already have account?{' '}
+        <Link href='/login' className='text-[#FDE047] font-semibold'>
+          Sign In
+        </Link>
+      </p>
+    </section>
+  );
+}
