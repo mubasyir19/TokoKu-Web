c2e9f2820b4165ae7680e7ded3671ff70d01bf0e
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2024-03-12 12:50:32 +0700

    add(searchPage): implement design

--- src/app/(menu)/search/page.jsx
+++ src/app/(menu)/search/page.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 export default function Search() {
   return (
-    <div>
-      <p>This is Search Page</p>
+    <div className='px-5'>
+      <h1 className='text-2xl font-semibold text-center'>Find Your Needs Here</h1>
+      <div className='form-group mt-6'>
+        <input
+          type='text'
+          name=''
+          className='w-full px-5 py-2.5 border-2 rounded-full border-[#D9D9D9]'
+          placeholder='Cari disini'
+        />
+      </div>
     </div>
   );
 }
